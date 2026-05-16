1503d750f641db86e801fb40257cf48e2631b895
gitmahin <nimahin25@gmail.com>
2026-05-16 19:44:32 +0000

    chore: add comments in clear.sh, fix file ext and rm .vscode

--- clear.sh
+++ clear.sh
@@ -1,4 +1,8 @@
-# !/bin/bash
+# This command will clear all executable files and empty directories in recursive
+# You can only use this script for only linux systems or bash supported systems
 
+# !/bin/bash
+# Find all compiled ELF binaries and delete them
 find . -type f -exec file {} \; | grep "ELF" | cut -d: -f1 | xargs rm -f
+# Find and delete all empty directories
 find . -type d -empty -delete
