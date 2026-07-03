7fd55badfb40d48895b9ab84c5795e2eb7baac4c
git-mahmood <2025100000379@seu.edu.bd>
2026-07-03 21:30:03 +0600

    Create 71A-Way Too Long Words.cpp

---
+++ mahmud_cpp/71A-Way Too Long Words.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string a;
+        cin >> a;
+
+        if (a.length() > 10) {
+            cout << a[0] << a.length() - 2 << a[a.length() - 1] << endl;
+        } else {
+            cout << a << endl;
+        }
+    }
+    return 0;
+}
