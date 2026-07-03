42841b09d3882c760416054730fad55bf80c8d0b
git-mahmood <2025100000379@seu.edu.bd>
2026-07-03 10:41:26 +0600

    Delete python directory

--- python/158A-Next Round.py
+++
@@ -1,8 +0,0 @@
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-count = 0
-target_score = a[k - 1] 
-for i in a:
-    if i >= target_score and i > 0:
-        count += 1
-print(count)
--- python/50A-Domino Pilling.py
+++
@@ -1,4 +0,0 @@
-m, n = map(int, input().split())
-area = m * n
-domino = area // 2
-print(domino)
