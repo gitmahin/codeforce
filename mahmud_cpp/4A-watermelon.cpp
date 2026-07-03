e39441069e52262405af1d63348254c7b06b166c
git-mahmood <2025100000379@seu.edu.bd>
2026-07-03 21:02:46 +0600

    Update 4A-watermelon.cpp

--- mahmud_cpp/4A-watermelon.cpp
+++ mahmud_cpp/4A-watermelon.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    bool found = false;
     int w;
     cin >> w;
 
-    
-    for (int i = 2; i <= w; i++) {
-        int y = w - i;
-        
-        if (i % 2 == 0 && y % 2 == 0) {
-            found = true;
-            break;
-        }
-    }
-
-    if (found == true) {
-        cout << "True";
+    if (w > 2 && w % 2 == 0) {
+        cout << "YES" << endl;
     } else {
-        cout << "False";
+        cout << "NO" << endl;
     }
 
     return 0;
